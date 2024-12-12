451348aa346fb131d14bf18308dbdb22f483769f
hanasaki <hanasakayui2022@gmail.com>
2024-12-12 18:22:51 +0800

    🔧 Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: true,
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/puppeteer-core',
+        'node_modules/puppeteer',
+      ],
+    },
+  },
+  webpack: (config) => {
+    config.optimization.minimize = true
+    return config
+  },
 }
 
 module.exports = nextConfig
