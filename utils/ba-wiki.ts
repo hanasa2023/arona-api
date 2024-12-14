9814fb2f8fce398ed415b0f231e5c04e0ab3352b
hanasaki <hanasakayui2022@gmail.com>
2024-12-14 16:29:46 +0800

    🔥 Delete ba-wiki.ts

--- utils/ba-wiki.ts
+++
@@ -1,8 +0,0 @@
-export class BAWiki {
-  baseUrl: string = 'https://gamekee.com/bawiki'
-  async getMainUrlFromTitle(title: string) {
-    const res = await fetch(`${this.baseUrl}`)
-    if (res.ok) {
-    }
-  }
-}
